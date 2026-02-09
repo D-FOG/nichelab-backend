f9e5a8ff878ceeebe112a9fb733305a52486c5bc
D-FOG <ogbondafavour9@gmail.com>
2026-02-09 13:31:15 +0100

    update to code

--- modules/products/models/products.model.js
+++ modules/products/models/products.model.js
@@ -60,11 +60,9 @@ const productSchema = new mongoose.Schema(
         userId: {
           type: mongoose.Schema.Types.ObjectId,
           ref: "User",
-          required: true,
         },
         rating: {
           type: Number,
-          required: true,
           min: 1,
           max: 5,
         },
